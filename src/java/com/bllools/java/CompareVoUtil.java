c077ae513bf995eb5b6fd40b502c680589d127ae
Bllose Chen <574931094@qq.com>
2022-01-19 10:13:52 +0800

    Create CompareVoUtil.java

---
+++ src/java/com/bllools/java/CompareVoUtil.java
@@ -0,0 +1,87 @@
+package com.bllools.java;
+
+import org.apache.cxf.common.util.StringUtils;
+import lombok.Data;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompareVoUtil {
+
+    @Data
+    static class compareResult{
+        String targetField;
+        String targetType;
+        String originField;
+        String originType;
+    }
+
+    public static List<compareResult> compare(Object originVo, Object targetVo){
+        List<compareResult> resultList = new ArrayList<>();
+        Field[] originFields = originVo.getClass().getDeclaredFields();
+        Field[] targetFields = targetVo.getClass().getDeclaredFields();
+
+        // 找出不同的， 源存在但是目标端没有的域
+        for(Field curField: originFields){
+            boolean same = false;
+            String targetName = "";
+            String targetType = "";
+
+            String fieldName = curField.getName();
+            String fieldType = curField.getType().getTypeName();
+            for(Field searchedField: targetFields){
+                String targetFieldName = searchedField.getName();
+                if(fieldName.equals(targetFieldName)){
+                    String targetFieldType = searchedField.getType().getTypeName();
+                    if(fieldType.equals(targetFieldType)){
+                        same = true;
+                        break;
+                    }else{
+                        targetName = targetFieldName;
+                        targetType = targetFieldType;
+                    }
+                }
+            }
+
+            if(!same) {
+                compareResult result = new compareResult();
+                result.originField = fieldName;
+                result.originType = fieldType;
+                if(!StringUtils.isEmpty(targetName)) {
+                    result.targetField = targetName;
+                    result.targetField = targetType;
+                }
+                resultList.add(result);
+            }
+        }
+
+        // 专注找目标端有，但是源没有的域
+        int amount = originFields.length;
+        for(Field tarField: targetFields){
+            String fieldName = tarField.getName();
+            if(fieldName.equals("applyNo")){
+                System.out.println("~!!");
+            }
+            String fieldType = tarField.getType().getTypeName();
+            boolean same = false;
+
+            for(int i = 0 ; i < amount; i ++){
+                String originName = originFields[i].getName();
+                if(fieldName.equals(originName)){
+                    same = true;
+                    break;
+                }
+            }
+
+            if(!same){
+                compareResult result = new compareResult();
+                result.targetField = fieldName;
+                result.targetType = fieldType;
+                resultList.add(result);
+            }
+        }
+
+        return resultList;
+    }
+}
